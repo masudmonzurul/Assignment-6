f82ae1d1773da1c8e9ca439012a791cea8c4c73d
Masud Monzurul Islam <masud@Masuds-MacBook-Air.local>
2026-05-22 19:20:15 +0600

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/A-6-DigiTools-Platform/',
+  base: '/Assignment-6/',
   plugins: [
     react(),
     tailwindcss()
